2f14c211470b8ac6a678d47e3278522846988f78
lotosbin <lotosbin@gmail.com>
2025-05-21 14:23:48 +0800

    feat: initialize Kotlin Multiplatform project structure with basic configurations and files

---
+++ yuanjing/composeApp/src/desktopMain/kotlin/com/yuanjingtech/main.kt
@@ -0,0 +1,13 @@
+package com.yuanjingtech
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "yuanjing",
+    ) {
+        App()
+    }
+}
---
+++ yuanjing/shared/src/jvmMain/kotlin/com/yuanjingtech/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.yuanjingtech
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()

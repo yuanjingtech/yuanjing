93273abb4ffd89b1d014d8d606673c98c20510e1
lotosbin <lotosbin@gmail.com>
2025-08-09 17:26:07 +0800

    build(iOS): 修复路径设置，确保 mise 可用

--- yuanjing/iosApp/ci_scripts/ci_post_clone.sh
+++ yuanjing/iosApp/ci_scripts/ci_post_clone.sh
@@ -5,3 +5,6 @@ MISE_PATH=$(brew --prefix mise)
 export PATH="$MISE_PATH/bin/:$PATH"
 mise --version
 mise install java@21
+
+brew install openjdk@21
+echo 'export PATH="/usr/local/opt/openjdk@21/bin:$PATH"' >> ~/.zshrc

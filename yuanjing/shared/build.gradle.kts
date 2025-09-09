ab5e593d46a7ce5632f365b44990d48f37497276
lotosbin <lotosbin@gmail.com>
2025-09-09 15:35:32 +0800

    feat: 添加 Compose 多平台和编译器插件

--- yuanjing/shared/build.gradle.kts
+++ yuanjing/shared/build.gradle.kts
@@ -5,6 +5,8 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.androidLibrary)
 }
 
